8d5b8ce8d475685966ca240b2464cb8d3aa73b20
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2022-06-14 15:42:49 +0700

    Initial commit

---
+++ b/add.test.ts
@@ -0,0 +1,46 @@
+const { AddCounter } = require("./add");
+
+describe("test function add", () => {
+  const inputTests = [
+    {
+      name: "should return 3",
+      input: [1, 2],
+      expected: 3,
+    },
+    {
+      name: "should return 15",
+      input: [10, 5],
+      expected: 15,
+    },
+    {
+      name: "should return 0",
+      input: [1, -1],
+      expected: 0,
+    },
+    {
+      name: "should return -7",
+      input: [-2, -5],
+      expected: -7,
+    },
+    {
+      name: "should return 0",
+      input: [0, 0],
+      expected: 0,
+    },
+  ];
+
+  inputTests.forEach((test) => {
+    it(test.name, () => {
+      // Arrange
+      const sum = AddCounter(...test.input);
+      // Assert
+      expect(sum).toBe(test.expected);
+    });
+  });
+});
+
+/* describe("test function", () => {
+  test("find prime 1 - 10", () => {
+    expect(findPrime(10)).toBe(3);
+  });
+}); */
---
+++ b/app.ts
@@ -0,0 +1,35 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = 3001;
+
+/* const isPrime = (num: number): boolean => {
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}; */
+
+export const findPrime = (n: number) => {
+  for (var counter = 0; counter <= n; counter++) {
+    var notPrime = false;
+    for (var i = 2; i <= counter; i++) {
+      if (counter % i === 0 && i !== counter) {
+        notPrime = true;
+      }
+    }
+    if (notPrime === false) {
+      console.log(counter);
+    }
+  }
+};
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at https://localhost:${port}`);
+});
